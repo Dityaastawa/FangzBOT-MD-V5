226e3d14c76a6fd24066c56710c7648c864a67db
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-08-11 15:26:55 +0800

    Add files via upload

--- plugins/info-websiteowner.js
+++ plugins/info-websiteowner.js
@@ -9,7 +9,7 @@ conn.sendMessage(m.chat, {
     index: 1,
     urlButton: {
       displayText: `Link`,
-      url: 'https://fangzbot.websites.co.in/'
+      url: 'https://dityaastawa.githuh.io'
     }
   }],
   footer: wm
